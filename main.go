29a3df4da1e87b9f611e53c4cfeca970e81bf0d6
Lee Briggs <lbriggs@apptio.com>
2017-09-14 11:13:41 -0700

    0.2.4 release

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ package main
 
 import "github.com/apptio/breakglass/cmd"
 
-const VERSION = "v0.2.3-a3b136b"
-const SOURCE_DATE = "2017-09-14T09:16:53-07:00"
+const VERSION = "v0.2.4-488437a"
+const SOURCE_DATE = "2017-09-14T11:12:58-07:00"
 
 func main() {
 	cmd.Execute(VERSION)
